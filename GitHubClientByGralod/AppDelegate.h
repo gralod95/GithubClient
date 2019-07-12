a066758e047a66c4f2c7bc23867c4951d4b595fa
gralod95 <gralod95@mail.ru>
2019-07-13 00:15:45 +0300

    Add files via upload

---
+++ GitHubClientByGralod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubClientByGralod
+//
+//  Created by 1 on 05/07/2019.
+//  Copyright © 2019 OdinokovG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
